1664946843b48fc3f005d40268418e998c05ddf5
pborisp <popovbs@gmail.com>
2025-03-05 00:53:33 +0300

    Full Task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        Author teodorDrizer = new Author("Теодор", "Драйзер");
+        Book finansist = new Book("Финансист", teodorDrizer, 1912);
+        Author adityaBhargava = new Author("Адитья", "Бхаргава");
+        Book grokaemAlgoritms = new Book("Грокаем алгоритмы 2", adityaBhargava, 2024);
+        System.out.println(finansist.getNameBook() + " " + teodorDrizer.getFirstName() + " " + teodorDrizer.getLastName() + " " + finansist.getYearPublication());
+        System.out.println(grokaemAlgoritms.getNameBook() + " " + adityaBhargava.getFirstName() + " " + adityaBhargava.getLastName() + " " + grokaemAlgoritms.getYearPublication());
+        grokaemAlgoritms.setYearPublication(2019);
+        System.out.println(grokaemAlgoritms.getNameBook() + " " + adityaBhargava.getFirstName() + " " + adityaBhargava.getLastName() + " " + grokaemAlgoritms.getYearPublication());
     }
 }
