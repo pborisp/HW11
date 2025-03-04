280a8ad5baaf35f694f20c51eaef04be46b26799
pborisp <popovbs@gmail.com>
2025-03-05 00:09:44 +0300

    Add classes Author & Book

---
+++ src/Author.java
@@ -0,0 +1,27 @@
+import org.w3c.dom.DOMStringList;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    private String nameBook;
+    private Author author;
+    private int yearPublication;
+
+    public Book(String nameBook, Author author, int yearPublication) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+
+    public String setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public Author setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+}
